6fba54a2e9b6c516a0c3cf8855ee25828beda3e1
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-06-03 23:39:14 +0300

    refactor: add pagination to findByRole and findByIsActive methods

--- src/main/java/gr/aueb/cf/schoolspring/repository/IUserRepository.java
+++ src/main/java/gr/aueb/cf/schoolspring/repository/IUserRepository.java
@@ -2,17 +2,19 @@ package gr.aueb.cf.schoolspring.repository;
 
 import gr.aueb.cf.schoolspring.core.enums.Role;
 import gr.aueb.cf.schoolspring.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface IUserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
     Optional<User> findByUsername(String username);
-    List<User> findByRole(Role role);
-    List<User> findByIsActive(Boolean isActive);
+    Optional<User> findByUuid(String uuid);
+    Page<User> findByRole(Role role, Pageable pageable);
+    Page<User> findByIsActive(Boolean isActive, Pageable pageable);
     Long countByRole(Role role);
 }
