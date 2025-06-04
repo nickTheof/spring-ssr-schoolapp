3931d599df3ea8261ff129b346cbf5d798683499
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-06-04 12:53:08 +0300

    feat: implement UserDetailsService interface

---
+++ src/main/java/gr/aueb/cf/schoolspring/authentication/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package gr.aueb.cf.schoolspring.authentication;
+
+import gr.aueb.cf.schoolspring.repository.IUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final IUserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow(
+                () -> new UsernameNotFoundException("User with username " + username + " not found.")
+        );
+    }
+}
