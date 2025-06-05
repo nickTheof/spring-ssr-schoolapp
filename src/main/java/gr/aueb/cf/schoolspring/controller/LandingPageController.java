9ec186a60925691d90648f7ce26931725b145eb5
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-06-05 13:08:55 +0300

    chore: rename controller get method

--- src/main/java/gr/aueb/cf/schoolspring/controller/LandingPageController.java
+++ src/main/java/gr/aueb/cf/schoolspring/controller/LandingPageController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LandingPageController {
 
     @GetMapping("/")
-    public String LandingPage() {
+    public String landingPage() {
         return "index";
     }
 
