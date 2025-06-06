4f8348e106cedcd583baae2b4f4c9809c6046647
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-06-06 17:37:27 +0300

    feat: add GET, POST endpoints for user insert by Admin panel

--- src/main/java/gr/aueb/cf/schoolspring/controller/AdminPanelController.java
+++ src/main/java/gr/aueb/cf/schoolspring/controller/AdminPanelController.java
@@ -1,18 +1,19 @@
 package gr.aueb.cf.schoolspring.controller;
 
+import gr.aueb.cf.schoolspring.core.exceptions.EntityAlreadyExistsException;
 import gr.aueb.cf.schoolspring.core.exceptions.EntityNotFoundException;
+import gr.aueb.cf.schoolspring.dto.UserInsertByAdminDTO;
 import gr.aueb.cf.schoolspring.dto.UserReadOnlyDTO;
 import gr.aueb.cf.schoolspring.service.IUserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -91,5 +92,45 @@ public class AdminPanelController {
             attrs.addFlashAttribute("errorMessage", "Προέκυψε σφάλμα κατά την διαγραφή του χρήστη με uuid " + uuid);
         }
         return "redirect:/admin/panel/users";
+    }
+
+    @GetMapping("/users/insert")
+    public String getUserInsertForm(Model model) {
+        List<String> allRoles = List.of("ADMIN", "EDITOR", "READER");
+        model.addAttribute("userInsertDTO", new UserInsertByAdminDTO("", "", "", Boolean.TRUE, ""));
+        model.addAttribute("allRoles", allRoles);
+        return "users-insert";
+    }
+
+    @PostMapping("/users/insert")
+    public String userInsert(
+            @Valid @ModelAttribute("userInsertDTO") UserInsertByAdminDTO dto,
+            BindingResult bindingResult,
+            Model model,
+            RedirectAttributes attrs
+    ) {
+        List<String> allRoles = List.of("ADMIN", "EDITOR", "READER");
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("allRoles", allRoles);
+            return "users-insert";
+        }
+
+        if (!dto.password().equals(dto.confirmPassword())) {
+            model.addAttribute("allRoles", allRoles);
+            bindingResult.rejectValue("confirmPassword", "password.mismatch", "Οι κωδικοί δεν ταιριάζουν.");
+            return "users-insert";
+        }
+
+        try {
+            UserReadOnlyDTO readOnlyDTO = userService.saveUser(dto);
+            LOGGER.info("Ο χρήστης με id={}, uuid={}, username={}, isActive={}, ρόλο={} εισήχθη επιτυχώς.", readOnlyDTO.id(), readOnlyDTO.uuid(), readOnlyDTO.username(), readOnlyDTO.isActive(), readOnlyDTO.role());
+            attrs.addFlashAttribute("successMessage", "Ο χρήστης με username: " + readOnlyDTO.username() + "δημιουργήθηκε με επιτυχία");
+            return "redirect:/admin/panel/users";
+        } catch (EntityAlreadyExistsException e) {
+            LOGGER.error("User with username={} not inserted", dto.username(), e);
+            model.addAttribute("allRoles", allRoles);
+            model.addAttribute("errorMessage", e.getMessage());
+            return "users-insert";
+        }
     }
 }
