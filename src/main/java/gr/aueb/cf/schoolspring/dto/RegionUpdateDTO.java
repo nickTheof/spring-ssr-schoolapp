2113032fad0ac376ebf0b3d7db3985132f69a454
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-06-06 20:35:55 +0300

    chore: add more bean validation

--- src/main/java/gr/aueb/cf/schoolspring/dto/RegionUpdateDTO.java
+++ src/main/java/gr/aueb/cf/schoolspring/dto/RegionUpdateDTO.java
@@ -1,10 +1,12 @@
 package gr.aueb.cf.schoolspring.dto;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record RegionUpdateDTO(
         Integer id,
         @NotNull(message = "Το όνομα περιοχής δεν μπορεί να είναι κενό.")
+        @Size(min = 2, message = "Το όνομα πρέπει να περιέχει τουλάχιστον 2 χαρακτήρες")
         String name
 ) {
 }
